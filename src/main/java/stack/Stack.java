73edd33f2ecd9630e1141f8f17324ea019a409df
RakockiW <w.rakocki.064@studms.ug.edu.pl>
2024-10-26 12:14:12 +0200

    added tests and functions

--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -3,13 +3,49 @@ public class Stack {
 	private String [] elements;
 
 	public void push(String element){
+		int n;
+		if (elements == null) {
+			n = 0;
+		} else {
+			n = elements.length;
+		}	
+
+		String [] tempElements = new String[n+1];
 		
+		for (int i=0; i<n; i++) {
+			tempElements[i] = elements[i];
+		}
+
+		tempElements[n] = element;
+
+		elements = tempElements;
 	}
-	public String pop(){
-		return "";
+	public String pop() {
+		int n;
+		if (elements == null) {
+			return null;
+		}
+
+		n = elements.length;
+
+		String element = elements[n-1];
+
+		String [] tempElements = new String[n-1];
+		
+		for (int i=0; i<n-1; i++) {
+			tempElements[i] = elements[i];
+		}
+
+		elements = tempElements; 
+
+		return element;
+
 	}
 
 	public String peek(){
-		return "";
+		if (elements == null) {
+			return null;
+		}
+		return elements[elements.length - 1];
 	}
 }
--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -11,7 +11,6 @@ public class StackTest {
 		Stack stack = new Stack();
 		stack.push("napis");
 		String result = stack.pop();
-
 		assertEquals("pop after push", "napis", result);
 	}
 
@@ -23,4 +22,54 @@ public class StackTest {
 
 		assertEquals("peek after push", "napis", result);
 	}
-}
+
+	@Test
+	public void testMultiplePush() {
+		Stack stack = new Stack();
+		stack.push("napis1");
+		String result1 = stack.peek();
+		stack.push("napis2");
+		String result2 = stack.peek();
+
+		assertEquals("First push", "napis1", result1);
+		assertEquals("Second push", "napis2", result2);
+	}
+
+	@Test 
+	public void testPeekAfterPeek() {
+		Stack stack = new Stack();
+		stack.push("napis");
+		String firstPeek = stack.peek();
+		String secondPeek = stack.peek();
+
+		assertEquals("peek after peek", firstPeek, secondPeek);	
+	}
+
+	@Test
+	public void testPeekAfterPop() {
+		Stack stack = new Stack();
+		stack.push("napis1");
+		stack.push("napis2");
+		stack.pop();
+		String result = stack.peek();
+
+		assertEquals("peek after pop", "napis1", result);
+	}
+
+	@Test
+	public void testPopOnEmptyStack() {
+		Stack stack = new Stack();
+		String result = stack.pop();
+
+		assertEquals("pop on empty stack", null, result);
+	}
+
+	@Test
+	public void testPeekOnEmptyStack() {
+		Stack stack = new Stack();
+		String result = stack.peek();
+
+		assertEquals("peek on empty stack", null, result);
+	}
+}	
+
